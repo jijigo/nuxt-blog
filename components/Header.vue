aef797c6042acd739e4d4eb92a4d0a9fa1cb0f0d
debbyji <jasmine820921@gmail.com>
2019-03-17 20:33:40 +0800

    revise default layout

---
+++ components/Header.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+  Header
+    b-navbar(toggleable="lg" type="light" variant="light")
+      b-navbar-brand(href="/") My Blog
+      b-navbar-toggle(target="nav_collapse")
+      b-collapse#nav_collapse(is-nav)
+        b-navbar-nav
+          b-nav-item(href="/posts") 文章列表
+</template>
